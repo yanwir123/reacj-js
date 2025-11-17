0d5207041137f12fed865d6b085844fb599e54e2
yanwir123 <aryifin08@gmail.com>
2025-11-17 13:33:48 +0700

    update

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./Navbar.css";
+import "./navbar.css";
 
 const Navbar: React.FC = () => {
   const navigate = useNavigate();
